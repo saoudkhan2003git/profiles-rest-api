8f08fa88e89efc0253a3957ba4c03a81ab2faf3d
Saoud <khansaoud@gmail.com>
2023-07-23 16:40:58 -0400

    Added ViewSet Methods

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 urlpatterns = [
-    path('hello_view/', views.HelloAppView.as_view()),
+    path('hello-view/', views.HelloAppView.as_view()),
+    path('',include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from profiles_api import serializers
 # Create your views here.
 
@@ -44,3 +46,46 @@ class HelloAppView(APIView):
         """handle delete an object"""
         return Response({'methohd':'DELETE'})
     
+class HelloViewSet(viewsets.ViewSet):
+    """Test APIViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+        """Return a message"""
+
+        a_viewset = [
+            'Use actions (list, create, retrieve, update, partial update)',
+            'Automatically map to urls using routers',
+            'Provide more functionality with less code',
+        ]
+        return Response({'message':'Hello','a_viewset':a_viewset})
+    
+    def create(self,request):
+        """View Set ceate method"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hell {name}!"
+            return Response({"message":message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        """Test retrieve method"""
+        return Response({'http_method':'retrieve'})
+    
+    def update(self,request,pk=None):
+        """Test update method"""
+        return Response({'http_method':'PUT'})
+    
+    def partial_update(self,request,pk=None):
+        """Test partial update"""
+        return Response({'http_method':'Patch'})
+
+    def destroy(self,request,pk=None):
+        """Test destroy method"""
+        return Response({'http_method':'DELETE'})
