a3cdf8bbf5e11d41d2b7e08b822eeb761311c57e
Saoud <khansaoud@gmail.com>
2023-07-23 17:22:44 -0400

    Added Users profiles and methods

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloAppView.as_view()),
     path('',include(router.urls))
