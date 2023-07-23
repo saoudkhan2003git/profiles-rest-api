f835d7fb8854f3e18849c1ee17dd20749b5b63d5
Saoud <khansaoud@gmail.com>
2023-07-22 23:05:36 -0400

    added customer user, manager and migration

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
